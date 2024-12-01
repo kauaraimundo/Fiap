f480972dba69e6bc5b159a63942645809f18644b
kauaraimundo <renatoishikawa@hotmail.com>
2024-12-01 02:18:00 -0300

    alteração script

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Fiap
 Projetos da Fiap
+
+1. Instalação do Python -  Acesse o Python.org - Clique em dowloads e baixe a última versão e faça a instalação padrão 
+
+2. Instalação da biblioteca OPENCV - pip install opencv-python
+
+3. Instalção da biblioteca DEEPFACE - pip install deepface
+
+4. Instalação da biblioteca TFKERAS - pip install tf-keras
+
+5. Instalação da biblioteca MEdiaPipe - pip install mediapipe
+
+6. O nome do video que será analisado deve ser video.mp4 e deve estar localizado no mesmo diretório do código techchallenger4.py
+
+7. O arquivo summary.txt deve estar na mesma pasta que o código techchallenger4.py
--- techchallenger4.py
+++ techchallenger4.py
@@ -4,9 +4,17 @@ import numpy as np
 from tqdm import tqdm
 from deepface import DeepFace
 import mediapipe as mp
+import math
 
+# Variáveis para contagem
+anomalous_movement_counter = 0
+analyzed_frames_counter = 0
+
+# Função que processa o vídeo 
 def proccess_video(video_path, output_path):
-
+    global analyzed_frames_counter
+    global anomalous_movement_counter
+    
     # Capturar vídeo do arquivo especificado
     cap = cv2.VideoCapture(video_path)
     
@@ -21,8 +29,6 @@ def proccess_video(video_path, output_path):
     fps = int(cap.get(cv2.CAP_PROP_FPS))
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    write_summary("Total de frames." + str(total_frames))
-
     # Definir o codec e criar o objeto VideoWriter
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Codec para MP4
     out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
@@ -30,6 +36,8 @@ def proccess_video(video_path, output_path):
     
     batch = []
     countBatch = 0
+
+    previous_landmarks_batch = None #landmarks anterior para detecção de movimentos anomalos 
     # Loop para processar cada frame do vídeo com barra de progresso
     for i in tqdm(range(total_frames), desc="Processando vídeo"):
         
@@ -43,64 +51,85 @@ def proccess_video(video_path, output_path):
         batch.append(frame)
 
         if len(batch) >= batch_size:
-            process_batch(batch, out, countBatch)
+            previous_landmarks_batch = process_batch(batch, out, countBatch, previous_landmarks_batch)
             countBatch = countBatch + 1
             batch = []  # Reinicia o batch após o processamento
 
     #processa os frames restantes        
     if batch:
-        countBatch = countBatch + 1
-        process_batch(batch, out, countBatch)
+        process_batch(batch, out, countBatch, previous_landmarks_batch)
 
     # Liberar a captura de vídeo e fechar todas as janelas
     cap.release()
     out.release()
+    write_summary("Total de Movimentos Anômalos: " + str(anomalous_movement_counter))
+    write_summary("Total de frames analisados: " + str(analyzed_frames_counter))
+    write_summary("Total de frames." + str(total_frames))
 
-def process_batch(batch, out, countBatch):
+#Função que processa os frames em pacotes para agilizar o processo 
+#e retorna o último frame processado para analise de movimentos anomalos 
+def process_batch(batch, out, countBatch, previous_landmarks_batch):
+    global analyzed_frames_counter
+    global anomalous_movement_counter
+
+    previous_landmarks = previous_landmarks_batch #landmarks anterior para detecção de movimentos anomalos 
+
     mp_pose = mp.solutions.pose
     pose = mp_pose.Pose(model_complexity=2,   # Nível mais alto de complexidade para maior precisão
-                        static_image_mode=False,         # False para utilizar o contexto de vídeo e melhorar o rastreamento
+                        static_image_mode=False, # False para utilizar o contexto de vídeo e melhorar o rastreamento
                         smooth_landmarks=True, # Suaviza os pontos para reduzir o ruído 
-                        min_detection_confidence = 0.7,    # Confiança mínima para detectar a pose
-                        min_tracking_confidence=0.5      # Confiança mínima para rastrear a pose
+                        min_detection_confidence = 0.7,  # Confiança mínima para detectar a pose
+                        min_tracking_confidence=0.5  # Confiança mínima para rastrear a pose
                         )
+
     mp_drawing = mp.solutions.drawing_utils 
 
     for i, frame in enumerate(batch):
         
         current_frame = i + (countBatch * 64) 
+         # Escreve o número do frame no canto superior esquerdo
+        text = f"Frame: {current_frame}"
+        position = (10, 30)  # Posição do texto (x, y)
+        font = cv2.FONT_HERSHEY_SIMPLEX  # Tipo de fonte
+        font_scale = 1  # Tamanho do texto
+        color = (0, 255, 0)  # Cor do texto (verde)
+        thickness = 2  # Espessura do texto
+        # Adiciona o texto ao frame
+        cv2.putText(frame, text, position, font, font_scale, color, thickness)
 
         if i % 4 == 0:  # analise de 4 em 4 frames  
-            
+            analyzed_frames_counter = analyzed_frames_counter + 1
+
             #Identificando as posições
             rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             results_pose = pose.process(rgb_frame)
             if results_pose.pose_landmarks :
-                movement_identified = False
                 mp_drawing.draw_landmarks(frame, results_pose.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-                if is_arm_up(results_pose.pose_landmarks.landmark, mp_pose):
-                   write_summary("Frame: " + str(current_frame) + " Braço levantado.")
-                   movement_identified = True
+                current_landmarks = results_pose.pose_landmarks.landmark
 
-                if is_person_lying(results_pose.pose_landmarks.landmark, mp_pose) : 
+                if is_arm_up(current_landmarks, mp_pose):
+                   write_summary("Frame: " + str(current_frame) + " Pessoa com os braços levantado.")
+
+                if is_person_lying(current_landmarks, mp_pose) : 
                     write_summary("Frame: " + str(current_frame) + " Pessoa deitada.")
-                    movement_identified = True
-                
-                if(is_arms_down(results_pose.pose_landmarks.landmark, mp_pose)):
-                    write_summary("Frame: " + str(current_frame) + " Pessoa com braços abaixados.")
-                    movement_identified = True
 
-                if(is_looking_forward(results_pose.pose_landmarks.landmark, mp_pose)):
-                    write_summary("Frame: " + str(current_frame) + " Pessoa olhando para frente")
-                    movement_identified = True    
+                if(is_arms_down(current_landmarks, mp_pose)):
+                    write_summary("Frame: " + str(current_frame) + " Pessoa com os braços abaixados.")
 
-                look_side = is_profile_view(results_pose.pose_landmarks.landmark, mp_pose)
+                if(is_looking_forward(current_landmarks, mp_pose)):
+                    write_summary("Frame: " + str(current_frame) + " Pessoa olhando para frente") 
+
+                look_side = is_profile_view(current_landmarks, mp_pose)
                 if look_side is not None:
-                    write_summary("Frame: " + str(current_frame) + " Pessoa olhando para o lado " + look_side)
-                    movement_identified = True    
+                    write_summary("Frame: " + str(current_frame) + " Pessoa olhando para o lado " + look_side)   
 
-                if(movement_identified is False):
-                    write_summary("Frame: Posição anômala, não identificada.")
+                if(previous_landmarks != None):
+                    anomalous_movement = is_anomalous_movement(current_landmarks, previous_landmarks)
+                    if(anomalous_movement):
+                        write_summary("Frame: " + str(current_frame) + " Movimento anômalo identificado.")
+                        anomalous_movement_counter = anomalous_movement_counter+1
+
+                previous_landmarks = current_landmarks
 
             # Analisar o frame para detectar faces e expressões
             # detector_backend = mtcnn Melhorou o reconhecimento de faces de lado 
@@ -120,21 +149,12 @@ def process_batch(batch, out, countBatch):
                 # Escrever a emoção dominante acima da face
                 cv2.putText(frame, dominant_emotion, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
                 if dominant_emotion:
-                    write_summary("Frame: " + str(current_frame) + ' ' + dominant_emotion)
-
-                # Escreve o número do frame no canto superior esquerdo
-                text = f"Frame: {current_frame}"
-                position = (10, 30)  # Posição do texto (x, y)
-                font = cv2.FONT_HERSHEY_SIMPLEX  # Tipo de fonte
-                font_scale = 1  # Tamanho do texto
-                color = (0, 255, 0)  # Cor do texto (verde)
-                thickness = 2  # Espessura do texto
-
-                # Adiciona o texto ao frame
-                cv2.putText(frame, text, position, font, font_scale, color, thickness)
+                    write_summary("Frame: " + str(current_frame) + ' Emoção detectada - ' + dominant_emotion)
 
         # Escrever o frame processado no vídeo de saída
         out.write(frame)
+
+    return previous_landmarks
 
 # Função para verificar se o braço está levantado
 def is_arm_up(landmarks, mp_pose):
@@ -227,6 +247,22 @@ def is_profile_view(landmarks, mp_pose):
 
     return None
 
+# Funçao para calcular a distância entre dois pontos 3D
+def calculate_distance_3d(p1, p2):
+    return math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2 + (p1.z - p2.z) ** 2)
+
+# Funçao para detectar movimentos Anomalos
+def is_anomalous_movement(current_landmarks, previous_landmarks, threshold=0.18):
+    if not previous_landmarks:
+        return False
+
+    # Calcula o movimento médio entre os pontos
+    movement = sum(
+        calculate_distance_3d(current, previous)
+        for current, previous in zip(current_landmarks, previous_landmarks)
+    ) / len(current_landmarks)
+
+    return movement > threshold
 
 # Função para escrever o resumo automático
 def write_summary(text):
@@ -239,9 +275,8 @@ with open("summary.txt", "w") as arquivo:
 
 # Caminho para o arquivo de vídeo na mesma pasta do script
 script_dir = os.path.dirname(os.path.abspath(__file__))
-input_video_path = os.path.join(script_dir, 'video.mp4') # video que será analisado
+input_video_path = os.path.join(script_dir, 'video.mp4')  # Substitua 'meu_video.mp4' pelo nome do seu vídeo
 output_video_path = os.path.join(script_dir, 'output_video.mp4')  # Nome do vídeo de saída
 
-# Chamar a função para detectar emoções e reconhecer faces e movimentos no vídeo, salvando o vídeo processado
+# Chamar a função para detectar emoções e reconhecer faces no vídeo, salvando o vídeo processado
 proccess_video(input_video_path, output_video_path)
-
